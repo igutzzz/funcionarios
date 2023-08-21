ebba563bf942bd2842cf03baea04dc98cecacb8d
igutzzz <igor.almeida@madeinweb.app>
2023-08-21 15:14:46 -0300

    first commit

---
+++ app/funcionario/[id]/page.js
@@ -0,0 +1,22 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export default function Funcionario({params}) {
+    const [funcionario, setFuncionario] = useState({})
+    useEffect(() => {
+        fetch(`http://129.148.27.50/api/funcionario/${params.id}`)
+        .then(res => res.json())
+        .then(res => setFuncionario(res))
+    }, [])
+    return (
+        <div>
+            <h1>{funcionario.nome}</h1>
+            <h2>{funcionario.cargo}</h2>
+            <div>
+                <p>CPF: {funcionario.cpf}</p>
+                <p>RG: {funcionario.rg}</p>
+            </div>
+        </div>
+    )
+}
