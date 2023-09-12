9f8db4d2a985fcd8f3d564a43f7993179a969178
igutzzz <ofc.iguituu@gmail.com>
2023-09-12 18:46:06 -0300

    integracao api

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,39 @@
+"use client"
+
 import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+
+    function doLogin(e, s) {
+        console.log(email, senha)
+        fetch("https://api-cadastro-clientes.onrender.com/login", {
+            method: "POST",
+            body: JSON.stringify({
+                email: e,
+                senha: s
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then((response) => response.json())
+        .then((data)=> console.log(data))
+    }
+
+    const handleEmail = e =>{
+        setEmail(e.target.value)
+        console.log(email)
+    }
+
+    const handleSenha = e => {
+        setSenha(e.target.value)
+    }
+    
     return(
         <main className="flex items-center justify-center h-screen  bg-gradient-to-tr from-purple-700 via-indigo-500 to-indigo-500 ">
             <div className="p-12 shadow-lg border-2 rounded-lg bg-slate-100 mx-4">
@@ -13,12 +44,12 @@ export default function Login() {
                 <div className="mt-12">
                     <div>
                         <label htmlFor="email" className="font-semibold">E-mail</label> <br />
-                        <input type="email" name="email" id="email" className="px-4 py-3 bg-slate-300 rounded-md mb-8 mt-2 w-full" /> <br />
+                        <input type="email" value={email} onChange={handleEmail} name="email" id="email" className="px-4 py-3 bg-slate-300 rounded-md mb-8 mt-2 w-full" /> <br />
                         <label htmlFor="password" className="font-semibold">Senha</label> <br />
-                        <input type="password" name="password" id="password" className="px-4 py-3 bg-slate-300 rounded-md mb-8 mt-2 w-full" />
+                        <input type="password" value={senha} onChange={handleSenha} name="password" id="password" className="px-4 py-3 bg-slate-300 rounded-md mb-8 mt-2 w-full" />
                     </div>
                     <a href="#" className="font-semibold underline text-purple-600 mb-2 ">Esqueci minha senha</a> <br />
-                    <button className={inter.className+" mt-12 bg-purple-600 w-full rounded-md text-center py-3 text-slate-50 transition-all ease-in-out hover:cursor-pointer hover:bg-purple-500"}>Acessar</button>
+                    <button onClick={doLogin(email, senha)} className={inter.className+" mt-12 bg-purple-600 w-full rounded-md text-center py-3 text-slate-50 transition-all ease-in-out hover:cursor-pointer hover:bg-purple-500"}>Acessar</button>
                 </div>
             </div>
         </main>
